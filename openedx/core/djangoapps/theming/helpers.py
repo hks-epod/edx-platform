825c6b350fcbbe561eed0aa9d78995788b1b8060
Zia Fazal <zia.fazal@arbisoft.com>
2016-02-18 19:07:31 +0500

    ziafazal/WL-328: Comprehensive theming enhancements to support multi-tenancy
    ziafazal: improvements need for multi-tenancy
    ziafazal: fixed broken tests
    ziafazal: no need to add setting in test.py
    ziafazal: added hostname validation
    ziafazal: changes after feedback from mattdrayer
    ziafazal: fixed branding and microsite broken tests
    ziafazal: make STATICFILES_DIRS to list
    ziafazal: added theme directory to mako lookup for tests
    ziafazal: added more protection in test_util

--- openedx/core/djangoapps/theming/helpers.py
+++ openedx/core/djangoapps/theming/helpers.py
@@ -1,9 +1,17 @@
 """
     Helpers for accessing comprehensive theming related variables.
 """
+import re
+import os.path
+from path import Path
+
+from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.sites.models import Site
+from django.core.cache import cache
+
 from microsite_configuration import microsite
 from microsite_configuration import page_title_breadcrumbs
-from django.conf import settings
 
 
 def get_page_title_breadcrumbs(*args):
@@ -24,7 +32,11 @@ def get_template_path(relative_path, **kwargs):
     """
     This is a proxy function to hide microsite_configuration behind comprehensive theming.
     """
-    return microsite.get_template_path(relative_path, **kwargs)
+    template_path = get_template_path_with_theme(relative_path)
+    if template_path == relative_path:  # we don't have a theme now look into microsites
+        template_path = microsite.get_template_path(relative_path, **kwargs)
+
+    return template_path
 
 
 def is_request_in_themed_site():
@@ -32,6 +44,14 @@ def is_request_in_themed_site():
     This is a proxy function to hide microsite_configuration behind comprehensive theming.
     """
     return microsite.is_request_in_microsite()
+
+
+def get_template(uri):
+    """
+    This is a proxy function to hide microsite_configuration behind comprehensive theming.
+    :param uri: uri of the template
+    """
+    return microsite.get_template(uri)
 
 
 def get_themed_template_path(relative_path, default_path, **kwargs):
@@ -52,3 +72,145 @@ def get_themed_template_path(relative_path, default_path, **kwargs):
     if is_stanford_theming_enabled and not is_microsite:
         return relative_path
     return microsite.get_template_path(default_path, **kwargs)
+
+
+def get_template_path_with_theme(relative_path):
+    """
+    Returns template path in current site's theme if it finds one there otherwise returns same path.
+    :param relative_path:
+    :return: template path in current site's theme
+    """
+    site_theme_dir = get_current_site_theme_dir()
+    if not site_theme_dir:
+        return relative_path
+
+    base_theme_dir = get_base_theme_dir()
+    root_name = get_project_root_name()
+    template_path = "/".join([
+        base_theme_dir,
+        site_theme_dir,
+        root_name,
+        "templates"
+    ])
+
+    # strip `/` if present at the start of relative_path
+    template_name = re.sub(r'^/+', '', relative_path)
+    search_path = os.path.join(template_path, template_name)
+    if os.path.isfile(search_path):
+        path = '/{site_theme_dir}/{root_name}/templates/{template_name}'.format(
+            site_theme_dir=site_theme_dir,
+            root_name=root_name,
+            template_name=template_name,
+        )
+        return path
+    else:
+        return relative_path
+
+
+def strip_site_theme_templates_path(uri):
+    """
+    :return: removes site templates theme path in uri
+    """
+    site_theme_dir = get_current_site_theme_dir()
+    if not site_theme_dir:
+        return uri
+
+    root_name = get_project_root_name()
+    templates_path = "/".join([
+        site_theme_dir,
+        root_name,
+        "templates"
+    ])
+
+    uri = re.sub(r'^/*' + templates_path + '/*', '', uri)
+    return uri
+
+
+def get_current_site_theme_dir():
+    """
+    :return: theme directory for current site
+    """
+    from edxmako.middleware import REQUEST_CONTEXT
+    request = getattr(REQUEST_CONTEXT, 'request', None)
+    if not request:
+        return None
+
+    # if hostname is not valid
+    if not all((isinstance(request.get_host(), basestring), is_valid_hostname(request.get_host()))):
+        return None
+
+    try:
+        site = get_current_site(request)
+    except Site.DoesNotExist:
+        return None
+    site_theme_dir = cache.get(get_site_theme_cache_key(site))
+
+    # if site theme dir is not in cache and comprehensive theming is enabled then pull it from db.
+    if not site_theme_dir and is_comprehensive_theming_enabled():
+        site_theme = site.themes.first()  # pylint: disable=no-member
+        if site_theme:
+            site_theme_dir = site_theme.theme_dir_name
+            cache_site_theme_dir(site, site_theme_dir)
+    return site_theme_dir
+
+
+def get_project_root_name():
+    """
+    :return: component name of platform e.g lms, cms
+    """
+
+    root = Path(settings.PROJECT_ROOT)
+    if root.name == "":
+        root = root.parent
+    return root.name
+
+
+def get_base_theme_dir():
+    """
+    :return: Base theme directory
+    """
+    return settings.COMPREHENSIVE_THEME_DIR
+
+
+def is_comprehensive_theming_enabled():
+    """
+    :return boolen: boolean indicating of comprehensive theming is enabled or not
+    """
+    return True if settings.COMPREHENSIVE_THEME_DIR else False
+
+
+def get_site_theme_cache_key(site):
+    """
+    :param site: site where key needs to generated
+    :return: a key to be used a cache key
+    """
+    cache_key = "theming.site.{domain}".format(
+        domain=site.domain
+    )
+    return cache_key
+
+
+def is_valid_hostname(hostname):
+    """
+    Returns boolean indicating if given hostname is valid or not
+    :param hostname:
+    :return:
+    """
+    if len(hostname) > 255 or "." not in hostname:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[:-1]  # strip exactly one dot from the right, if present
+    if ":" in hostname:
+        hostname = hostname.split(":")[0]  # strip port number if present
+
+    allowed = re.compile(r"(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
+
+
+def cache_site_theme_dir(site, theme_dir):
+    """
+    Cache site's theme directory.
+    :param site:
+    :param theme_dir:
+    """
+    cache.set(get_site_theme_cache_key(site), theme_dir, settings.FOOTER_CACHE_TIMEOUT)
--- openedx/core/djangoapps/theming/test_util.py
+++ openedx/core/djangoapps/theming/test_util.py
@@ -6,47 +6,36 @@ from functools import wraps
 import os
 import os.path
 import contextlib
+import re
 
 from mock import patch
 
 from django.conf import settings
-from django.template import Engine
-from django.test.utils import override_settings
+from django.contrib.sites.models import Site
 
 import edxmako
-
-from .core import comprehensive_theme_changes
-
-EDX_THEME_DIR = settings.REPO_ROOT / "themes" / "edx.org"
+from .models import SiteTheme
 
 
-def with_comprehensive_theme(theme_dir):
+def with_comprehensive_theme(theme_dir_name):
     """
-    A decorator to run a test with a particular comprehensive theme.
-
+    A decorator to run a test with a comprehensive theming enabled.
     Arguments:
-        theme_dir (str): the full path to the theme directory to use.
-            This will likely use `settings.REPO_ROOT` to get the full path.
-
+        theme_dir_name (str): directory name of the site for which we want comprehensive theming enabled.
     """
-    # This decorator gets the settings changes needed for a theme, and applies
-    # them using the override_settings and edxmako.paths.add_lookup context
-    # managers.
-
-    changes = comprehensive_theme_changes(theme_dir)
-
+    # This decorator creates Site and SiteTheme models for given domain
     def _decorator(func):                       # pylint: disable=missing-docstring
         @wraps(func)
         def _decorated(*args, **kwargs):        # pylint: disable=missing-docstring
-            with override_settings(COMPREHENSIVE_THEME_DIR=theme_dir, **changes['settings']):
-                default_engine = Engine.get_default()
-                dirs = default_engine.dirs[:]
-                with edxmako.save_lookups():
-                    for template_dir in changes['template_paths']:
-                        edxmako.paths.add_lookup('main', template_dir, prepend=True)
-                        dirs.insert(0, template_dir)
-                    with patch.object(default_engine, 'dirs', dirs):
-                        return func(*args, **kwargs)
+            # make a domain name out of directory name
+            domain = "{theme_dir_name}.org".format(theme_dir_name=re.sub(r"[\s.org_]", "", theme_dir_name))
+            site, __ = Site.objects.get_or_create(domain=domain, name=domain)
+            SiteTheme.objects.get_or_create(site=site, theme_dir_name=theme_dir_name)
+            edxmako.paths.add_lookup('main', settings.COMPREHENSIVE_THEME_DIR, prepend=True)
+            with patch('openedx.core.djangoapps.theming.helpers.get_current_site_theme_dir',
+                       return_value=theme_dir_name):
+                with patch('openedx.core.djangoapps.theming.helpers.get_current_site', return_value=site):
+                    return func(*args, **kwargs)
         return _decorated
     return _decorator
 
@@ -64,7 +53,7 @@ def with_is_edx_domain(is_edx_domain):
     def _decorator(func):                       # pylint: disable=missing-docstring
         if is_edx_domain:
             # This applies @with_comprehensive_theme to the func.
-            func = with_comprehensive_theme(EDX_THEME_DIR)(func)
+            func = with_comprehensive_theme('edx.org')(func)
 
         return func
 
@@ -81,12 +70,13 @@ def with_edx_domain_context(is_edx_domain):
 
     """
     if is_edx_domain:
-        changes = comprehensive_theme_changes(EDX_THEME_DIR)
-        with override_settings(COMPREHENSIVE_THEME_DIR=EDX_THEME_DIR, **changes['settings']):
-            with edxmako.save_lookups():
-                for template_dir in changes['template_paths']:
-                    edxmako.paths.add_lookup('main', template_dir, prepend=True)
-
+        domain = 'edx.org'
+        site, __ = Site.objects.get_or_create(domain=domain, name=domain)
+        SiteTheme.objects.get_or_create(site=site, theme_dir_name=domain)
+        edxmako.paths.add_lookup('main', settings.COMPREHENSIVE_THEME_DIR, prepend=True)
+        with patch('openedx.core.djangoapps.theming.helpers.get_current_site_theme_dir',
+                   return_value=domain):
+            with patch('openedx.core.djangoapps.theming.helpers.get_current_site', return_value=site):
                 yield
     else:
         yield
