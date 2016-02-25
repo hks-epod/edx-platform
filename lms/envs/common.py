8603a53c255d335ccad2d51d1a4f0cdb4d067241
Nimisha Asthagiri <nasthagiri@edx.org>
2016-02-25 11:05:41 -0500

    Activate Next and Previous Buttons across sections MA-2152 MA-2153
    (cherry picked from commit 8cbf99aca4274d51c57486a67b63015e3b488d2b)

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -397,6 +397,9 @@ FEATURES = {
 
     # Enable LTI Provider feature.
     'ENABLE_LTI_PROVIDER': False,
+
+    # Enable Next Button to jump sequences in Sequence Navigation bar.
+    'ENABLE_NEXT_BUTTON_ACROSS_SECTIONS': False,
 }
 
 # Ignore static asset files on import which match this pattern
